4f9d6e41f91d65779093849c0a78e7cc3133f113
Emil Madsen <sovende@gmail.com>
2023-10-31 17:43:36 +0100

    feat: [#56025] update to GraphQL v19

--- job_function_configurator/config.py
+++ job_function_configurator/config.py
@@ -1,12 +1,15 @@
 # SPDX-FileCopyrightText: 2023 Magenta ApS <https://magenta.dk>
 # SPDX-License-Identifier: MPL-2.0
 from fastramqpi.config import Settings as FastRAMQPISettings
+from pydantic import PositiveInt
 
 
 class JobFunctionSettings(FastRAMQPISettings):
     """Settings for the engagement elevator AMQP trigger."""
 
     log_level: str = "INFO"
+
+    mo_graphql_version: PositiveInt = 19
 
     # HELPERS
     email_user_key_for_address_type: list = []
